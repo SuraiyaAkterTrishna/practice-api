654bf26a3ac076d79d90ae10b3f044702a1442b1
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-08 15:07:18 +0600

    load and display 20 comments done

--- js/practise1.js
+++ js/practise1.js
@@ -2,9 +2,19 @@ const loadComments = () => {
     const url = 'https://jsonplaceholder.typicode.com/comments';
     fetch(url)
     .then(res => res.json())
-    .then(data => displayComments(data));
+    .then(data => displayComments(data.slice(0, 20)));
 };
 const displayComments = (comments) => {
-    console.log(comments);
+    const commentsContainer = document.getElementById('comments-container');
+    comments.forEach(comment => {
+        console.log(comment);
+        const commentsDiv = document.createElement('div');
+        commentsDiv.innerHTML = `
+            <h1>Title: ${comment.name}</h1>
+            <h3>Email: ${comment.email}</h3>
+            <p>Description: ${comment.body}</p>
+        `;
+        commentsContainer.appendChild(commentsDiv);
+    });
 };
 loadComments();
