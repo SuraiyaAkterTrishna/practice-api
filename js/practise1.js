51281a30fb615a05b83709dcfc316c2835503be1
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-08 15:27:32 +0600

    practice 1 done

--- js/practise1.js
+++ js/practise1.js
@@ -7,14 +7,31 @@ const loadComments = () => {
 const displayComments = (comments) => {
     const commentsContainer = document.getElementById('comments-container');
     comments.forEach(comment => {
-        console.log(comment);
+        // console.log(comment);
         const commentsDiv = document.createElement('div');
+        commentsDiv.classList.add('comment');
         commentsDiv.innerHTML = `
             <h1>Title: ${comment.name}</h1>
             <h3>Email: ${comment.email}</h3>
             <p>Description: ${comment.body}</p>
+            <button onclick = "loadDetails(${comment.postId})">Details</button>
         `;
         commentsContainer.appendChild(commentsDiv);
     });
 };
+const loadDetails = (postId) => {
+    const url = `https://jsonplaceholder.typicode.com/posts/${postId}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayDetails(data));
+};
+const displayDetails = (post) => {
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = `
+        <h1>Post Details</h3>
+        <h3>Title: ${post.title}</h1>
+        <h3>Post No: ${post.id}</h3>
+        <p>Description: ${post.body}</p>
+    `;
+}
 loadComments();
